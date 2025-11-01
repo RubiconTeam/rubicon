4ee5d4a6d36863509d4dd663c1a9f4abd42a9092
anniebuue <anniebuue@proton.me>
2025-10-31 23:28:57 -0500

    Game: Added note settings and judgments

---
+++ scripts/data/game/rubicon_level_note_hit_result.gd
@@ -0,0 +1,9 @@
+class_name RubiconLevelNoteHitResult extends Object
+
+enum Judgment {
+	JUDGMENT_PERFECT = 1 << 0,
+	JUDGMENT_GREAT = 1 << 1,
+	JUDGMENT_GOOD = 1 << 2,
+	JUDGMENT_OKAY = 1 << 3,
+	JUDGMENT_BAD = 1 << 4
+}
---
+++ scripts/data/game/rubicon_level_note_settings.gd
@@ -0,0 +1,58 @@
+@tool
+class_name RubiconLevelNoteSettings extends Resource
+
+@export_range(0, 100, 1.0, "or_greater") var starting_health : int = 50
+
+@export_group("Judgments", "judgment_")
+@export_flags("Perfect", "Great", "Good", "Okay", "Bad") var judgment_enabled : int:
+	get:
+		return _judgments_enabled
+	set(val):
+		_judgments_enabled = val
+		notify_property_list_changed()
+
+@export_subgroup("Windows", "judgment_window_")
+@export var judgment_window_perfect : float = 20
+@export var judgment_window_great : float = 45
+@export var judgment_window_good : float = 75
+@export var judgment_window_okay : float = 105
+@export var judgment_window_bad : float = 130
+
+var _judgments_enabled : int = 63
+
+func _validate_property(property : Dictionary) -> void:
+	if property.name.begins_with("judgment_window_"):
+		var judgment_name : String = property.name.replace("judgment_window_", "")
+		match judgment_name:
+			"perfect":
+				_validate_judgment(property, judgment_name, RubiconLevelNoteHitResult.Judgment.JUDGMENT_PERFECT)
+			"great":
+				_validate_judgment(property, judgment_name, RubiconLevelNoteHitResult.Judgment.JUDGMENT_GREAT)
+			"good":
+				_validate_judgment(property, judgment_name, RubiconLevelNoteHitResult.Judgment.JUDGMENT_GOOD)
+			"okay":
+				_validate_judgment(property, judgment_name, RubiconLevelNoteHitResult.Judgment.JUDGMENT_OKAY)
+			"bad":
+				_validate_judgment(property, judgment_name, RubiconLevelNoteHitResult.Judgment.JUDGMENT_BAD)
+
+func _property_can_revert(property: StringName) -> bool:
+	if property == "judgment_enabled":
+		return true
+	
+	return false
+
+func _property_get_revert(property : StringName) -> Variant:
+	if property == "judgment_enabled":
+		return 63
+	
+	return
+
+func _validate_judgment(property : Dictionary, judgment_name : String, judgment_value : RubiconLevelNoteHitResult.Judgment) -> void:
+	if property.name == "judgment_window_%s" % judgment_name:
+		if not _check_for_judgment(judgment_enabled, judgment_value):
+			property.usage = PROPERTY_USAGE_NONE
+		else:
+			property.usage = PROPERTY_USAGE_DEFAULT
+
+func _check_for_judgment(flags : int, judgment : RubiconLevelNoteHitResult.Judgment) -> bool:
+	return flags & judgment == judgment
