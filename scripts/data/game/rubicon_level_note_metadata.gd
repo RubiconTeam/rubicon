3c66f1b2dcb2dc1a197d3ce0c7ffc981c3fb1f55
anniebuue <anniebuue@proton.me>
2025-11-07 05:23:39 -0600

    Notes: In the process of creating note metadata

---
+++ scripts/data/game/rubicon_level_note_metadata.gd
@@ -0,0 +1,7 @@
+class_name RubiconLevelNoteMetadata extends Resource
+
+@export var scene : PackedScene
+
+@export_group("Predefines", "predefine_")
+@export var predefine_should_miss : bool = false
+@export var predefine_count_towards_score : bool = true
