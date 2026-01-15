9b0b93a5526b0f8eab156dc45cda5e41f3ee0084
legole0 <legole0@proton.me>
2026-01-15 06:42:52 -0300

    Levels: Level playtesting prototype

--- plugin.gd
+++ plugin.gd
@@ -5,6 +5,9 @@ class_name RubiconPlugin
 const CREATE_CONTEXT_MENU_PLUGIN = preload("res://addons/rubicon/scripts/create_context_menu_plugin.gd")
 var _instance:EditorContextMenuPlugin
 
+var playtesting_level:bool = false
+var playtest_checkbox:CheckBox
+
 func _enter_tree() -> void:
 	add_project_setting("rubicon/defaults/note_database", TYPE_STRING, PROPERTY_HINT_FILE, "*.tres,*.res", "res://note_database.tres")
 	add_project_setting("rubicon/defaults/character_template", TYPE_STRING, PROPERTY_HINT_FILE, "*.tscn", "")
@@ -17,8 +20,15 @@ func _enter_tree() -> void:
 	_instance.connect("popup_menu", _popup_menu)
 	
 	add_context_menu_plugin(EditorContextMenuPlugin.CONTEXT_SLOT_FILESYSTEM_CREATE, _instance)
+	
+	playtest_checkbox = CheckBox.new()
+	playtest_checkbox.text = "Playtest Level (PROTOTYPE)"
+	playtest_checkbox.connect("pressed", playtest_checked)
+	
+	connect("scene_changed", _scene_changed)
 
 func _exit_tree() -> void:
+	playtest_checkbox.queue_free()
 	remove_context_menu_plugin(_instance)
 
 func add_project_setting(name : String, type : Variant.Type, hint : PropertyHint, hint_string : String = "", default_value : Variant = null, basic : bool = true) -> void:
@@ -49,3 +59,31 @@ func _create_popup(paths:PackedStringArray, popup_scene:PackedScene, confirm_cal
 	add_child(instance)
 	instance.popup_centered(instance.size * DisplayServer.screen_get_scale())
 	instance.confirmed.connect(func(): confirm_call.call(instance.base_path, instance._name))
+
+func _scene_changed(scene_root:Node) -> void:
+	if scene_root is RubiconLevel2D or scene_root is RubiconLevel3D:
+		add_control_to_container(CONTAINER_CANVAS_EDITOR_MENU, playtest_checkbox)
+	else:
+		disable_playtesting()
+		remove_control_from_container(CONTAINER_CANVAS_EDITOR_MENU, playtest_checkbox)
+
+func playtest_checked() -> void:
+	if get_tree() != null and !(get_tree().edited_scene_root is RubiconLevel2D or get_tree().edited_scene_root is RubiconLevel3D):
+		return
+	
+	playtesting_level = playtest_checkbox.button_pressed
+
+func disable_playtesting() -> void:
+	playtesting_level = false
+	playtest_checkbox.set_pressed_no_signal(false)
+	EditorInterface.get_editor_viewport_2d().gui_disable_input = false
+
+func _handles(object: Object) -> bool:
+	return true
+
+func _forward_canvas_gui_input(event: InputEvent) -> bool:
+	if event is InputEventKey and playtesting_level:
+		EditorInterface.get_editor_viewport_2d().gui_disable_input = false
+		EditorInterface.get_editor_viewport_2d().push_input(event)
+		return true
+	return false
