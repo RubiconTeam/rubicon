13e265be00ec3c8f24c0dfdd2e385204a4ef636c
cherry <138817584+cherrythecool@users.noreply.github.com>
2026-04-10 00:10:24 -0400

    LevelClock: Cache measure, beat, and step by default for optimization

--- scripts/scene/rubicon_level_clock.gd
+++ scripts/scene/rubicon_level_clock.gd
@@ -9,30 +9,31 @@ class_name RubiconLevelClock extends Node
 		var is_on_animation : bool = animation_player != null and (animation_player.is_animation_active() or animation_player.is_playing())
 		if not is_on_animation:
 			return 0
-		
+
 		return (_animation_player.current_animation_position + offset) * 1000.0
 	set(val):
 		var is_on_animation : bool = animation_player != null and (animation_player.is_animation_active() or animation_player.is_playing())
 		if not is_on_animation:
 			return
-		
+
 		_animation_player.seek((val / 1000.0) - offset)
+		recalculate_cache()
 
 @export var time_measure : float:
 	get:
-		return RubiconTimeChange.get_measure_at_millisecond(get_time_changes(), time_milliseconds)
+		return _cached_measure
 	set(val):
 		time_milliseconds = RubiconTimeChange.get_millisecond_at_measure(get_time_changes(), val)
 
 @export var time_beat : float:
 	get:
-		return RubiconTimeChange.get_beat_at_millisecond(get_time_changes(), time_milliseconds)
+		return _cached_beat
 	set(val):
 		time_milliseconds = RubiconTimeChange.get_millisecond_at_beat(get_time_changes(), val)
 
 @export var time_step : float:
 	get:
-		return RubiconTimeChange.get_step_at_millisecond(get_time_changes(), time_milliseconds)
+		return _cached_step
 	set(val):
 		time_milliseconds = RubiconTimeChange.get_millisecond_at_step(get_time_changes(), val)
 
@@ -50,21 +51,35 @@ var _animation_player : AnimationPlayer
 var _current_frame_time : float
 var _relative_time_offset : float
 
-var _last_step:int = -1
-var _last_beat:int = -1
 var _last_measure:int = -1
+var _last_beat:int = -1
+var _last_step:int = -1
+var _last_time_change: RubiconTimeChange
 
-signal step_change
-signal beat_change
+var _cached_measure: float
+var _cached_beat: float
+var _cached_step: float
+
 signal measure_change
+signal beat_change
+signal step_change
 
 func get_time_precise() -> float:
 	return _current_frame_time + (Time.get_unix_time_from_system() - _relative_time_offset) * 1000.0
 
+func get_measure_precise() -> float:
+	return RubiconTimeChange.get_measure_at_millisecond(get_time_changes(), get_time_precise())
+
+func get_beat_precise() -> float:
+	return RubiconTimeChange.get_beat_at_millisecond(get_time_changes(), get_time_precise())
+
+func get_step_precise() -> float:
+	return RubiconTimeChange.get_step_at_millisecond(get_time_changes(), get_time_precise())
+
 func get_time_changes() -> Array[RubiconTimeChange]:
 	if _level != null and _level.metadata != null:
 		return _level.metadata.time_changes
-	
+
 	return []
 
 func _validate_property(property: Dictionary) -> void:
@@ -74,19 +89,20 @@ func _validate_property(property: Dictionary) -> void:
 func _process(delta: float) -> void:
 	_current_frame_time = time_milliseconds
 	_relative_time_offset = Time.get_unix_time_from_system()
-	
+	recalculate_cache()
+
 	if step_change.has_connections():
 		var cur_step:int = floori(time_step)
 		if cur_step != _last_step:
 			_last_step = cur_step
 			step_change.emit()
-			
+
 	if beat_change.has_connections():
 		var cur_beat:int = floori(time_beat)
 		if cur_beat != _last_beat:
 			_last_beat = cur_beat
 			beat_change.emit()
-			
+
 	if measure_change.has_connections():
 		var cur_measure:int = floori(time_measure)
 		if cur_measure != _last_measure:
@@ -95,22 +111,25 @@ func _process(delta: float) -> void:
 
 func _notification(what: int) -> void:
 	match what:
+		NOTIFICATION_READY:
+			recalculate_cache()
+
 		NOTIFICATION_PARENTED:
 			if _level != null:
 				_level.clock = null
 				_level = null
-			
+
 			var parent : Node = get_parent()
 			if parent is RubiconLevel:
 				_level = parent
-				
+
 				if _level.clock == null:
 					_level.clock = self
 				else:
 					printerr(tr("WARNING!! Having more than one RubiconLevelClock in a level can lead to problems!"))
-				
+
 				return
-			
+
 		NOTIFICATION_CHILD_ORDER_CHANGED:
 			_animation_player = null
 			for child in get_children():
@@ -120,14 +139,26 @@ func _notification(what: int) -> void:
 
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings : PackedStringArray
-	
+
 	if level == null:
 		warnings.append(tr("This node relies on being parented to a RubiconLevel! Please parent this node to a RubiconLevel node"))
-	
+
 	if _animation_player == null:
 		warnings.append(tr("No AnimationPlayer child found! The clock relies on its first AnimationPlayer child to function."))
-	
+
 	return warnings
+
+func recalculate_cache() -> void:
+	_cached_measure = RubiconTimeChange.get_measure_at_millisecond(get_time_changes(), time_milliseconds)
+	_cached_beat = RubiconTimeChange.get_beat_at_millisecond(get_time_changes(), time_milliseconds)
+	_cached_step = RubiconTimeChange.get_step_at_millisecond(get_time_changes(), time_milliseconds)
+
+	var changes := get_time_changes()
+	for change: RubiconTimeChange in changes:
+		if change.measure <= time_measure:
+			_last_time_change = change
+		else:
+			break
 
 static func measure_to_millisecond(measure : float, bpm : float, time_signature_numerator : float) -> float:
 	return measure * ((60000.0 / bpm) * time_signature_numerator)
