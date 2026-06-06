a4fc5b6905ab6266634382adf292fe8548aab78d
Annie <30415792+anniebuue@users.noreply.github.com>
2026-06-06 07:20:48 -0500

    ScrollVelocity: Fix getting position

--- scripts/data/chart/rubichart_scroll_velocity.gd
+++ scripts/data/chart/rubichart_scroll_velocity.gd
@@ -13,13 +13,14 @@ func initialize(time_changes : Array[RubiconTimeChange]) -> void:
 
 func initialize_with_previous(time_changes : Array[RubiconTimeChange], last_velocity : RubiChartScrollVelocity) -> void:
 	initialize(time_changes)
-	position = last_velocity.position + (millisecond_time - last_velocity.millisecond_time) * last_velocity.multiplier
+	position = last_velocity.position + ((millisecond_time - last_velocity.millisecond_time) * last_velocity.multiplier)
 
 static func get_graphic_position_at_millisecond(velocities : Array[RubiChartScrollVelocity], millisecond_time : float) -> float:
-	for current in velocities:
-		if millisecond_time < current.millisecond_time:
-			continue
-		
-		return current.position + ((millisecond_time - current.millisecond_time) * current.multiplier)
+	var vel_index: int = velocities.size() - 1
+	for i in velocities.size():
+		if velocities[i].millisecond_time > millisecond_time:
+			vel_index = i - 1
+			break
 	
-	return 0
+	var velocity: RubiChartScrollVelocity = velocities[vel_index]
+	return velocity.position + ((millisecond_time - velocity.millisecond_time) * velocity.multiplier)
